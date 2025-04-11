90d5654ab19873213519fbb514a6ba71f42f4dd4
Tran Van Cong <aanh7108@gmail.com>
2025-04-11 20:59:55 +0700

    bai router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import {BrowserRouter} from "react-router-dom"
+import RouterCustom from './router';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <h1>VanCog</h1>
+    <BrowserRouter>
+        <RouterCustom />
+    </BrowserRouter>
 );
 
 
---
+++ src/pages/user/homeProfile/index.js
@@ -0,0 +1,13 @@
+import {memo} from "react";
+
+
+const HomeProfile= () => {
+    return(
+        <>  
+
+             <h1>HomeProfile</h1>
+        </>
+    )
+}
+
+export default memo(HomeProfile);
